54c48d09e34ef7e81238b8e3964fbc0ccc05f663
Lakshita-Goyal <146127187+Lakshita-Goyal@users.noreply.github.com>
2025-02-05 13:46:06 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,17 @@
+public class EvenorOdd 
+{
+    public static void main(String[] args)
+    {
+        int num=97;
+
+        if(num%2==0)
+        {
+           System.out.println("Number is even");
+        }
+        else
+        {
+           System.out.println("Number is odd");
+        }
+    }
+    
+}
